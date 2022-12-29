db7c1171ecc1936f65137fac21f672b8a3da687f
Achalogy <achalogy@gmail.com>
2022-12-28 21:09:22 -0500

    🚀 New API

--- src/youtubeApi/getMyVideos.js
+++ src/youtubeApi/getMyVideos.js
@@ -1,16 +1,8 @@
 const getMyVideos = async() => {
-  const x = await fetch(`https://www.googleapis.com/youtube/v3/search?key=${process.env.YOUTUBE_API}&channelId=UCdHxbtl_C3vMzpHCnNWf66Q&part=snippet,id&order=date&maxResults=100`);
+  const x = await fetch(`https://back-achaweb.vercel.app/youtube-videos`);
   let res = await x.json()
 
-  console.log(res)
-
-  return res.items.filter(x => x.id.kind == "youtube#video").map(x => {
-    return {
-      id: x.id.videoId,
-      title: x.snippet.title,
-      video: `https://youtu.be/${x.id.videoId}`
-    }
-  })
+  return res
 }
 
 export default getMyVideos
