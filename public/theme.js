60ad92c3937e4dd0639a72aba523fb20aab422ae
Achalogy <achalogy@gmail.com>
2022-12-30 23:43:35 -0500

    ⚡️ Mejorado theme.js

--- public/theme.js
+++ public/theme.js
@@ -1,7 +1,3 @@
-; (function initTheme() {
-  var theme = localStorage.getItem('theme') || 'light'
-  if (theme === 'dark') {
-    document.querySelector('html').classList.add('dark')
-    document.body.style.backgroundColor = "#1F2232"
-  }
-})()
+(function initTheme(){var theme=localStorage.getItem('theme')||'light'
+if(theme==='dark'){document.querySelector('html').classList.add('dark')
+document.body.style.backgroundColor="#1F2232"}})()
