04a088fdc4363e04a0fda1a045c2697646fd479a
Achalogy <achalogy@gmail.com>
2022-12-31 20:37:07 -0500

    ✨ Add footer

--- src/layouts/main.layout.tsx
+++ src/layouts/main.layout.tsx
@@ -1,3 +1,7 @@
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import { faCat, faPaw } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useRouter } from "next/dist/client/router"
 import ThemeSwitch from "../components/theme"
 
 export default function MainLayout({ children }: any) {
@@ -7,19 +11,26 @@ export default function MainLayout({ children }: any) {
     ["Blog", "/blog"]
   ]
 
+  const router = useRouter()
+
   return(
     <div className="dark:bg-darkMode-900">
       <header className="sticky top-0 lg:relative z-10 drop-shadow-sm bg-white dark:bg-darkMode-900 dark:text-white py-5 lg:px-16 flex flex-row justify-center lg:justify-between dark:drop-shadow-md">
-        <a className="text-2xl hidden lg:inline" href="/#Achalogy">{"</Achalogy"}</a>
+        <p className="text-2xl hidden lg:inline cursor-pointer" onClick={() => router.push("/")}>{"</Achalogy"}</p>
         <div className="flex flex-row gap-8 items-center">
           <ThemeSwitch />
           {menus.map(menu => 
-            <a key={menu[1]} id={menu[1]} href={`${menu[1]}`} className="font-medium text-lg">{menu[0]}</a>
+            <a key={menu[1]} id={menu[1]} href={`${menu[1]}`} className="font-medium text-lg no-underline">{menu[0]}</a>
           )}
         </div>
       </header>
       {children}
-      <footer className="h-half"></footer>
+      <footer className="h-half flex items-center justify-center text-xl text-white">
+        <p>Check this repo at <a className="text-sky-300 tracking-wider" onClick={() => window.open('https://github.com/achalogy/achaweb')}>
+            Github Repo <FontAwesomeIcon icon={faGithub} />
+          </a>
+        </p>
+      </footer>
     </div>
   )
 }
