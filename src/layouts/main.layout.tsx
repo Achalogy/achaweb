8ade45b4b3710e32ccae27d14e873b376f3d0c8f
Achalogy <achalogy@gmail.com>
2023-01-11 21:18:15 -0500

    📱 Fix Responsive Navbar

--- src/layouts/main.layout.tsx
+++ src/layouts/main.layout.tsx
@@ -13,24 +13,42 @@ export default function MainLayout({ children }: any) {
 
   const router = useRouter()
 
-  return(
+  return (
     <div className="dark:bg-darkMode-900">
       <header className="sticky top-0 lg:relative z-10 drop-shadow-sm bg-white dark:bg-darkMode-900 dark:text-white py-5 lg:px-16 flex flex-row justify-center lg:justify-between dark:drop-shadow-md">
-        <p className="text-2xl hidden lg:inline cursor-pointer" onClick={() => router.push("/")}>{"</Achalogy"}</p>
-        <div className="flex flex-row gap-8 items-center">
+        <p
+          className="text-2xl hidden lg:inline cursor-pointer"
+          onClick={() => router.push("/")}
+        >
+          {"</Achalogy"}
+        </p>
+        <div className="flex flex-row justify-evenly items-center w-full lg:gap-8 lg:justify-end">
           <ThemeSwitch />
-          {menus.map(menu => 
-            <a key={menu[1]} id={menu[1]} href={`${menu[1]}`} className="font-medium text-lg no-underline">{menu[0]}</a>
-          )}
+          {menus.map((menu) => (
+            // Mejorar Responsive (Agregar menu lateral)
+            <a
+              key={menu[1]}
+              id={menu[1]}
+              href={`${menu[1]}`}
+              className="font-medium text-sm lg:text-lg no-underline"
+            >
+              {menu[0]}
+            </a>
+          ))}
         </div>
       </header>
       {children}
       <footer className="h-half flex items-center justify-center text-xl text-white">
-        <p>Check this repo at <a className="text-sky-300 tracking-wider" onClick={() => window.open('https://github.com/achalogy/achaweb')}>
+        <p>
+          Check this repo at{" "}
+          <a
+            className="text-sky-300 tracking-wider"
+            onClick={() => window.open("https://github.com/achalogy/achaweb")}
+          >
             Github Repo <FontAwesomeIcon icon={faGithub} />
           </a>
         </p>
       </footer>
     </div>
-  )
+  );
 }
