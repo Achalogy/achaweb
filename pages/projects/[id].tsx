d5004b87eec8d71e8ec426cef419e420dd6f3d31
Achalogy <achalogy@gmail.com>
2023-01-16 23:50:07 -0500

    🐛 Algo esta mal con prod

--- pages/projects/[id].tsx
+++ pages/projects/[id].tsx
@@ -1,43 +1,41 @@
 import getProps from "../../src/api/getProps";
-import getProjectsList from '../../src/api/projects/getProjectsList'
-import getProject from '../../src/api/projects/getProject.js'
+import getProjectsList from "../../src/api/projects/getProjectsList";
+import getProject from "../../src/api/projects/getProject.js";
 
-export default function ProjectPage({project}: any) {
-  return <p>{project.name}</p>
+export default function ProjectPage({ project }: any) {
+  return <p>{JSON.stringify(project)}</p>;
 }
 
 export async function getStaticPaths() {
-  const projects = await getProjectsList()
+  const projects = await getProjectsList();
   const projectList = await projects.map((p: any) => {
     return {
       params: {
-        id: (p.githubName)
-      }
-    }
-  })
+        id: p.githubName,
+      },
+    };
+  });
   return {
     paths: projectList,
     fallback: true,
-  }
+  };
 }
 
 export async function getStaticProps(context: any) {
-  const { id } = context.params
-  const project = await getProject(id)
-  
-  if(!project) {
+  const { id } = context.params;
+  const project = await getProject(id);
+
+  if (!project) {
     return {
-      notFound: true
-    }
+      notFound: true,
+    };
   }
 
   return {
     props: {
       ...(await getProps()).props,
-      project
+      project,
     },
-    revalidate: 900
-  }
-  
-  return await getProps();
+    revalidate: 900,
+  };
 }
