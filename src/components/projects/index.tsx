33e4b8615243ec39e64e8d31225ebed66e920bb7
Achalogy <achalogy@gmail.com>
2023-01-17 21:50:55 -0500

    ✨ Project List

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,23 +1,9 @@
 import { faBookOpenReader, faStar } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
+import getProjectsLists from "../../api/projects/getProjectsList";
 
-const Project = () => {
-
-  const [stars, setStars] = useState()
-
-  const getStars = async () => {
-    const { stargazers_count } = await(
-      await fetch("https://api.github.com/repos/achalogy/advent-js-2022")
-    ).json();
-    
-    setStars(stargazers_count)
-  }
-
-  useEffect(() => {
-    getStars()
-  }, [])
-
+const Project = ({ name, stars, shortDescription }: any) => {
   return (
     <div
       className="flex flex-col px-4 py-10 gap-5 drop-shadow-xl rounded-lg bg-white dark:bg-darkMode-800 justify-center items-center text-center w-full lg:w-1/4 hover:bg-gray-50 cursor-pointer dark:text-white"
@@ -31,21 +17,41 @@ const Project = () => {
       <div className="rounded-full bg-yellow-400 aspect-square w-20 flex items-center justify-center text-4xl text-white">
         <FontAwesomeIcon icon={faBookOpenReader} />
       </div>
-      <h1 className="font-semibold text-lg">Advent Js 2022 for newbies</h1>
-      <p className="leading-5 text-sm dark:text-zinc-400">
-        Step by Step analisis and explanation of Advent Js challenges.
-      </p>
+      <h1 className="font-semibold text-lg">{name}</h1>
+      <p className="leading-5 text-sm dark:text-zinc-400">{shortDescription}</p>
       <p className="flex flex-row items-center gap-2 px-4 p-1 rounded-full border border-stone-600">
-        <FontAwesomeIcon icon={faStar} className="text-yellow-300 stroke-orange-300 stroke-[2em]" />
+        <FontAwesomeIcon
+          icon={faStar}
+          className="text-yellow-300 stroke-orange-300 stroke-[2em]"
+        />
         {stars}
       </p>
     </div>
   );
-}
-
+};
 
 export default function Projects() {
-  return(
-    <Project />
-  )
+  const [projects, setProjects]: any[] = useState();
+
+  const getProjects = async () => {
+    const data = await getProjectsLists();
+    setProjects(data);
+  };
+
+  useEffect(() => {
+    getProjects();
+  }, []);
+
+  return (
+    <>
+      {projects &&
+        projects.map(({ name, stars, shortDescription }: any) => (
+          <Project
+            name={name}
+            starts={stars}
+            shortDescription={shortDescription}
+          />
+        ))}
+    </>
+  );
 }
