05c6b179f22f5e43bb356c2ab987ac896b2e7b80
Achalogy <achalogy@gmail.com>
2023-01-28 21:21:33 -0500

    Adsense

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-import { useEffect } from 'react'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+import { useEffect } from "react";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script
+          async
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE}`}
+          strategy="lazyOnload"
+          crossOrigin="anonymous"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
