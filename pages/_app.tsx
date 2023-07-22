83efaac21fbdbc1bb6b6ad3f808300b1da5e256a
Achalogy <achalogy@gmail.com>
2023-07-21 23:53:01 -0500

    Stats

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
+import { useEffect } from "react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (window.location.href.startsWith("https")) try { const o = { referer: document.referrer, userAgentData: (window as any).navigator.userAgentData, userAgent: window.navigator.userAgent, url: { href: window.location.href, origin: window.location.origin, pathname: window.location.pathname, params: window.location.search }, host: window.location.host, timestamp: Date.now() }, t = Buffer.from(JSON.stringify(o)).toString("base64"); fetch("https://admin.achalogy.dev/api/check", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify({ a: t }) }) } catch (o) { console.log(o) }
+  }, [pageProps])
+
   return (
     <div className="bg-slate-800 text-white font-serif">
       <Component {...pageProps} />
